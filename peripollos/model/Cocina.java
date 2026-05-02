2f0da33cbfc7505b7a63d1f543bd425b2b3f01de
lxionel <lioneldavora1@gmail.com>
2026-05-01 21:12:35 -0500

    Reformat Cocina.java with consistent spacing

--- peripollos/model/Cocina.java
+++ peripollos/model/Cocina.java
@@ -5,20 +5,17 @@ public class Cocina {
     private String nombreArea;
     private String descripcion;
 
-    // Constructor completo
     public Cocina(int idCocina, String nombreArea, String descripcion) {
         this.idCocina = idCocina;
         this.nombreArea = nombreArea;
         this.descripcion = descripcion;
     }
 
-    // Constructor sin ID
     public Cocina(String nombreArea, String descripcion) {
         this.nombreArea = nombreArea;
         this.descripcion = descripcion;
     }
 
-    // Getters y Setters
     public int getIdCocina() { return idCocina; }
     public void setIdCocina(int idCocina) { this.idCocina = idCocina; }
 
